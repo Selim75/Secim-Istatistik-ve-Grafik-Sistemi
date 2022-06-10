86e1a7403a5dfd1e997a8eda8f89b70414639bb2
Selim <102211400+Selim75@users.noreply.github.com>
2022-06-10 16:43:04 +0300

    Seçim İstatistik ve Grafik Sistemi
    C# ile Veri tabanı kullanılarak partilerin aldığı oyların progress barda istatistiksel olarak gösterimi yapılmıştır.

---
+++ Veri_Tabanli_Parti_Secim_Grafik_Istatistik/Veri_Tabanli_Parti_Secim_Grafik_Istatistik/FrmGrafikler.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Veri_Tabanli_Parti_Secim_Grafik_Istatistik
+{
+    public partial class FrmGrafikler : Form
+    {
+        public FrmGrafikler()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection baglanti = new SqlConnection(@"Data Source=DESKTOP-GH9F2IR;Initial Catalog=DBSECİMPROJE;Integrated Security=True");
+
+        private void FrmGrafikler_Load(object sender, EventArgs e)
+        {
+            // İlçe Adlarını Comboboxa Çekme
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("Select ILCEAD from TBLILCE", baglanti);
+            SqlDataReader dr = komut.ExecuteReader();
+            while (dr.Read())
+            {
+                comboBox1.Items.Add(dr[0]);
+            }
+            baglanti.Close();
+
+            // Grafiğe Toplam sonuçları Getirme, GroupBy komutu kullnılacak
+            baglanti.Open();
+            SqlCommand komut2 = new SqlCommand("Select SUM(APARTI),SUM(BPARTI),SUM(CPARTI),SUM(DPARTI),SUM(EPARTI) FROM TBLILCE", baglanti);
+            SqlDataReader dr2 = komut2.ExecuteReader();
+            while (dr2.Read())
+            {
+                chart1.Series["Partiler"].Points.AddXY("A PARTİ", dr2[0]);
+                chart1.Series["Partiler"].Points.AddXY("B PARTİ", dr2[1]);
+                chart1.Series["Partiler"].Points.AddXY("C PARTİ", dr2[2]);
+                chart1.Series["Partiler"].Points.AddXY("D PARTİ", dr2[3]);
+                chart1.Series["Partiler"].Points.AddXY("E PARTİ", dr2[4]);
+
+            }
+            baglanti.Close();
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("Select * From TBLILCE WHERE ILCEAD=@P1", baglanti);
+            komut.Parameters.AddWithValue("@P1", comboBox1.Text);
+            SqlDataReader dr = komut.ExecuteReader();
+            while (dr.Read())
+            {
+                progressBar1.Value = int.Parse(dr[2].ToString()); // A parti
+                progressBar2.Value = int.Parse(dr[3].ToString()); // B parti
+                progressBar3.Value = int.Parse(dr[4].ToString()); // C parti
+                progressBar4.Value = int.Parse(dr[5].ToString()); // D parti
+                progressBar5.Value = int.Parse(dr[6].ToString()); // E parti
+
+                LblA.Text = dr[2].ToString(); // A partinin sonunda yazan değeri
+                LblB.Text = dr[3].ToString(); // B
+                LblC.Text = dr[4].ToString(); // C
+                LblD.Text = dr[5].ToString(); // D
+                LblE.Text = dr[6].ToString(); // E
+            }
+            baglanti.Close();
+        }
+    }
+}
